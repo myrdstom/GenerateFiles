638ddbbaa64b74f52bba9b6a76c29722932d29fa
myrdstom <nserekopaul@gmail.com>
2019-08-22 12:34:47 +0300

    Set up PDF generation and retrival in the backend

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const pdf = require('html-pdf');
 const cors = require('cors');
+const pdfTemplate = require('./documents');
 
 const app = express();
 
@@ -11,4 +12,20 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+// POST- PDF generation and fetching of the data
+app.post('./create-pdf', (req,res) =>{
+    pdf.create(pdfTemplate(req.data), {}).toFile('result.pdf', (err) =>{
+        if(err){
+            return Promise.reject();
+        }
+        return Promise.resolve()
+    })
+})
+
+//GET - Send the generated PDF to the client
+app.get('/fetch-pdf', (req,res) =>{
+    res.sendFile(`${__dirname}/result.pdf`)
+})
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});

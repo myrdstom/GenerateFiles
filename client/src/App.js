c6483b4afd9283b8a86142570b44ddf0af094447
myrdstom <nserekopaul@gmail.com>
2019-08-22 12:10:10 +0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { saveAs } from 'file-server';
+
+import './App.css';
+
+class App extends Component {
+    state = {
+        name: '',
+        receiptId: 0,
+        price1: 0,
+        price2: 0,
+    };
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  createAndDownloadPDF = () =>{
+    axios.post('/create-pdf', this.state)
+  }
+    render() {
+        return (
+            <div className="App">
+                <input
+                    type="text"
+                    placeholder="Name"
+                    name="name"
+                    onChange={this.handleChange}
+                />
+                <input
+                    type="text"
+                    placeholder="Receipt ID"
+                    name="receiptId"
+                    onChange={this.handleChange}
+                />
+                <input
+                    type="text"
+                    placeholder="Price 1"
+                    name="price1"
+                    onChange={this.handleChange}
+                />
+                <input
+                    type="text"
+                    placeholder="Price 2"
+                    name="price2"
+                    onChange="this.handleChange"
+                />
+                <button onClick={this.createAndDownloadPDF}>
+                    Download PDF
+                </button>
+            </div>
+        );
+    }
+}
+
+export default App;
